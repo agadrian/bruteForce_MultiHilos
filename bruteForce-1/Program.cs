2ce3d7d5eca0d1f80e1e2b9f6118ad6e8677fccf
agadrian <arroyogomezadrian@gmail.com>
2025-01-21 21:10:15 +0100

    Part 1 finished

--- bruteForce-1/Program.cs
+++ bruteForce-1/Program.cs
@@ -1,2 +1,54 @@
 ﻿
 
+using System.Security.Cryptography;
+using System.Text;
+
+
+string path = "../../../passwords.txt";
+List<string> passwordsFile = File.ReadAllLines(path).ToList();
+
+Random rnd = new Random();
+List<string> passwordsList = passwordsFile.OrderBy(x => rnd.Next()).Take(100).ToList();
+
+
+// TEST: Mostrar la lista random que ha elegido
+foreach (string test in passwordsList)
+{
+    Console.Write($"{test}, ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+// Password random de la lista
+string randomPassword = passwordsList[rnd.Next(passwordsList.Count)];
+Console.WriteLine($"Password random: {randomPassword}");
+
+// La password random hasheada
+string hashedPassword = HashPass(randomPassword);
+Console.WriteLine($"Hashed password: {hashedPassword}");
+
+
+foreach (string password in passwordsList)
+{
+    string checkHash = HashPass(password);
+
+    if (checkHash == hashedPassword)
+    {
+        Console.WriteLine($"Contraseña encontrada: {password}");
+        Console.WriteLine($"Hash encontrado: {checkHash}");
+    }
+}
+
+
+// Hashear string a sha256  
+static string HashPass(string password)
+{
+    using (SHA256 sha256Hash = SHA256.Create())
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(password);
+        byte[] hash = sha256Hash.ComputeHash(bytes);
+        return Convert.ToBase64String(hash);
+        
+    }
+}
