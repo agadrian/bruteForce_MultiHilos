7a94d80fe6ab9d07a8c402672e40ef8714b6049c
agadrian <arroyogomezadrian@gmail.com>
2025-02-13 16:40:48 +0100

    Pool de hilos Added.

--- bruteForce-1/Program.cs
+++ bruteForce-1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace bruteForce_1;
+﻿using System.Collections.Concurrent;
+
+namespace bruteForce_1;
 
 using System.Diagnostics;
 
@@ -17,7 +19,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+
         Stopwatch sw = new Stopwatch();
         sw.Start();
 
@@ -28,7 +30,7 @@ public class Program
         String rndPassword = passwordsFile[rnd.Next(0, passwordsFile.Count)];
         Console.WriteLine("Password: " + rndPassword);
 
-        
+
         // La password random hasheada
         string hashedPassword = Utils.HashPass(rndPassword);
         Console.WriteLine($"Hashed password: {hashedPassword}");
@@ -36,60 +38,134 @@ public class Program
 
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         CancellationToken token = cancellationTokenSource.Token;
-        
-        Wrapper<Action> finalizar = new Wrapper<Action>(() => { });
-        
-        int numHilos = 6; //Environment.ProcessorCount ;
-        int totalPasswords = passwordsFile.Count;
-        int batchSize = totalPasswords / numHilos;
-        
-        Console.WriteLine($"Total passwords: {totalPasswords}. Batchsize: {batchSize}. Num hilos: {numHilos}");
-        
-        List<MiHilo> hilos = new List<MiHilo>();
 
-        for (int i = 0; i < numHilos; i++)
+
+        ParallelOptions options = new ParallelOptions
         {
-            int inicio = i * batchSize;
-            int fin;
+            MaxDegreeOfParallelism = Environment.ProcessorCount,
+            CancellationToken = token
+        };
+        
+        Console.WriteLine($"Hilos disponibles: {Environment.ProcessorCount}");
+        
+        ConcurrentDictionary<int, int> threadTasksCounter = new ConcurrentDictionary<int, int>();
 
-            // Si es el ultimo hilo, toma desde el indice de inicio hasta el indice del ultimo elemento de la lista
-            if (i == numHilos - 1)
-            {
-                fin = passwordsFile.Count;
-            }
-            else // Si no es el ultimo, toma 'batchsize' contraseñas
-            {
-                fin = inicio + batchSize;
-            }
-            
-            // (fin - inicio) en vez de (inicio - batchsize), porque el ultimo hilo pilla un num disintos de passwords que el resto de hilos
-            // Index incluido (incio = 10, count = 10 => Comprueba 10-19)
-            List<String> batch = passwordsFile.GetRange(inicio, fin - inicio);
-            
-            hilos.Add(
-                new MiHilo(
-                    sw,
-                    (i + 1).ToString(),
-                    finalizar,
-                    batch,
-                    hashedPassword,
-                    token,
-                    cancellationTokenSource
-                )
+        try
+        {
+            Parallel.ForEach(passwordsFile, options, (password, state, index) =>
+                {
+                    // Obtener id del hilo
+                    int threadId = Thread.CurrentThread.ManagedThreadId;
+                    threadTasksCounter.AddOrUpdate(threadId, 1, (k, v) => v + 1);
+
+                    //Console.WriteLine($"Hilo #{threadId} procesando: {password}");
+
+                    // Cancelar ejecucion de las tareas cuando se encuentre la password
+                    if (token.IsCancellationRequested)
+                    {
+                        state.Stop();
+                        return;
+                    }
+
+                    string passwordToCheckHashed = Utils.HashPass(password);
+
+                    if (passwordToCheckHashed == hashedPassword)
+                    {
+                        Console.WriteLine($"Password found: {password} en el índice {index} por el hilo {threadId}");
+                        // Cancelar tanto el token como el state para detener nuevas iteraciones y tareas en curso
+                        cancellationTokenSource.Cancel();
+                        state.Stop();
+                        
+                        sw.Stop();
+                        Console.WriteLine($"Tiempo total: {sw.Elapsed} ");
+                    }
+                }
             );
-            
-            // Mostrar las contraseñas asignadas al hilo actual
-            Console.WriteLine($"Hilo {i+1} procesará {batch.Count} contraseñas");
         }
-
-        foreach (var hilo in hilos)
+        catch (OperationCanceledException e)
         {
-            hilo.Start();
+            Console.WriteLine($"Busqueda cancelada. Todos los hilos se detuvieron.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error inesperado: {e.Message}");
+        }
+        
+        
+        Console.WriteLine($"Reparto de tareas por hilo:");
+        foreach (var hilo in threadTasksCounter)
+        {
+            Console.WriteLine($"Hilo {hilo.Key} procesó: {hilo.Value} contraseñas");
         }
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+/*
+Wrapper<Action> finalizar = new Wrapper<Action>(() => { });
+
+int numHilos = 6; //Environment.ProcessorCount ;
+int totalPasswords = passwordsFile.Count;
+int batchSize = totalPasswords / numHilos;
+
+Console.WriteLine($"Total passwords: {totalPasswords}. Batchsize: {batchSize}. Num hilos: {numHilos}");
+
+List<MiHilo> hilos = new List<MiHilo>();
+
+for (int i = 0; i < numHilos; i++)
+{
+    int inicio = i * batchSize;
+    int fin;
+
+    // Si es el ultimo hilo, toma desde el indice de inicio hasta el indice del ultimo elemento de la lista
+    if (i == numHilos - 1)
+    {
+        fin = passwordsFile.Count;
+    }
+    else // Si no es el ultimo, toma 'batchsize' contraseñas
+    {
+        fin = inicio + batchSize;
+    }
+
+    // (fin - inicio) en vez de (inicio - batchsize), porque el ultimo hilo pilla un num disintos de passwords que el resto de hilos
+    // Index incluido (incio = 10, count = 10 => Comprueba 10-19)
+    List<String> batch = passwordsFile.GetRange(inicio, fin - inicio);
+
+    hilos.Add(
+        new MiHilo(
+            sw,
+            (i + 1).ToString(),
+            finalizar,
+            batch,
+            hashedPassword,
+            token,
+            cancellationTokenSource
+        )
+    );
+
+    // Mostrar las contraseñas asignadas al hilo actual
+    Console.WriteLine($"Hilo {i+1} procesará {batch.Count} contraseñas");
+}
+
+foreach (var hilo in hilos)
+{
+    hilo.Start();
+}
+}
+}
+
+*/
     
 
 
